a92119c7c0799c891bb648a307c0008dbfef67d0
Chitransh Jain <cjsmcs2810@gmail.com>
2020-08-06 21:20:06 +0530

    app icon added. app ready.

--- lib/style/theme.dart
+++ lib/style/theme.dart
@@ -5,8 +5,8 @@ import 'package:flutter/cupertino.dart';
 class MyColors {
   const MyColors();
 
-  static const Color loginGradientEnd = const Color(0xFFf7418c);
-  static const Color loginGradientStart = const Color(0xFFfbab66);
+  static const Color loginGradientEnd = const Color(0xFF008c83);
+  static const Color loginGradientStart = const Color(0xFF76e1cd);
   static const Color cardBackground = const Color(0xFFFFC3DA);
 
   static const primaryGradient = const LinearGradient(
